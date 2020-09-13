e107423e6832b386f480c3aed20c83bcbc09ba5a
Vansh Luthra <vanshluthra2000@gmail.com>
2020-09-13 11:54:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import DisplayWeather from "./components/DisplayWeather";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coordinates: {
+        latitude: 28.7041,
+        longitude: 77.1025,
+        //Just giving some default values, incase geolocation cant be fetched on any device
+      },
+      data: {},
+      inputData: "",
+    };
+  }
+
+  componentDidMount() {
+    //We will use a HTML5 object(or API) called Navigator to know user location when app first loads
+    if (navigator.geolocation) {
+      //console.log("Supported")
+      navigator.geolocation.getCurrentPosition((position) => {
+        let newCoordinates = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+
+        this.setState({ coordinates: newCoordinates });
+
+        axios
+          .get(
+            `http://api.weatherstack.com/current?access_key=9b589bec07539a0a2aac5836fb5c6906&query=${this.state.coordinates.latitude},${this.state.coordinates.longitude}`
+          )
+          .then((response) => {
+            //console.log(response)
+            let weatherData = {
+              location: response.data.location.name,
+              temperature: response.data.current.temperature,
+              description: response.data.current.weather_descriptions[0],
+              region: response.data.location.region,
+              country: response.data.location.country,
+              wind_speed: response.data.current.wind_speed,
+              pressure: response.data.current.pressure,
+              precip: response.data.current.precip,
+              humidity: response.data.current.humidity,
+              img: response.data.current.weather_icons,
+            };
+
+            this.setState({ data: weatherData });
+          })
+          .catch((error) => console.log(error.message));
+      });
+    } else {
+      console.log("Not Supported");
+    }
+  }
+
+  //track input field
+  change = (value) => {
+    //console.log("Changing")
+    this.setState({ inputData: value });
+  };
+
+  changeWeather = (event) => {
+    event.preventDefault();
+    //api call
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=9b589bec07539a0a2aac5836fb5c6906&query=${this.state.inputData}`
+      )
+      .then((response) => {
+        //console.log(response)
+        let weatherData = {
+          location: response.data.location.name,
+          temperature: response.data.current.temperature,
+          description: response.data.current.weather_descriptions[0],
+          region: response.data.location.region,
+          country: response.data.location.country,
+          wind_speed: response.data.current.wind_speed,
+          pressure: response.data.current.pressure,
+          precip: response.data.current.precip,
+          humidity: response.data.current.humidity,
+          img: response.data.current.weather_icons,
+        };
+
+        this.setState({ data: weatherData });
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar changeRegion={this.change} changeWeather={this.changeWeather} />
+        <div className="container">
+          <DisplayWeather weather={this.state.data} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
